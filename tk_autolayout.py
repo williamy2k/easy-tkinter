18bd6755dc2b1a4fd804b267b6efb6c7951bdc27
williamy2k <44499245+williamy2k@users.noreply.github.com>
2022-10-17 07:37:17 +0100

    Update tk_autolayout.py

--- tk_autolayout.py
+++ tk_autolayout.py
@@ -1,34 +1,136 @@
+import collections
 from tkinter import *
 import tkinter.font as tkfont
 import yamliny
 import threading
+from pprint import pprint
+
 
 class Label(Label):
     def text(self, new_text=None):
-        if not new_text:
-            return self['text']
-        else:
+        if new_text:
             self['text'] = new_text
+        return self['text']
+
 
 class Button(Button):
     def on_click(self, cmd=None):
         if cmd is not None:
             self['command'] = cmd
 
+
+# Subclass which allows for functions to be applied to multiple elements without having to manually iterate
+class ElemCollection(list):
+    def __getattr__(self, name):
+        def wrapper(*args):
+            methods_to_run = []
+            for elem in self:
+                if hasattr(elem, name):
+                    if callable(getattr(elem, name)):
+                        methods_to_run.append(getattr(elem, name))
+            return [method(args) for method in methods_to_run]
+
+        return wrapper
+
+
+def clean_inline_yaml(inline_yaml):
+    yaml_in_progress = inline_yaml
+
+    # Begin by stripping lead/trail whitespace
+    lines = yaml_in_progress.split("\n")
+    lines = list(filter(None, lines))
+
+    # Capture indentation depth
+    indent_depth = 0
+    for i, char in enumerate(lines[0]):
+        if char in ' \t':
+            indent_depth = i + 1
+        else:
+            break
+
+    # Delete indent from each line
+    # for line in yaml_in_progress
+    print(indent_depth)
+
+    indent_stripped_lines = []
+    for line in lines:
+        indent_stripped_lines.append(line[indent_depth:])
+
+    yaml_in_progress = "\n".join(indent_stripped_lines)
+
+    cleaned_yaml = yaml_in_progress
+    return cleaned_yaml
+
+presentation_defaults = '''
+        window:
+          layout-mode: auto
+          width: 300
+          margin-left: 20
+          margin-top: 20
+          margin-right: 20
+          margin-bottom: 20
+        
+        header:
+          font-family: SFPro
+          font-size: 22
+          font-weight: bold
+          foreground-color: black
+          background-color: None
+          margin-top: 5
+          margin-bottom: 5
+          margin-left: 5
+          margin-right: 5
+          width: 150
+        
+        button:
+          height: 28
+          width: 100
+          font-family: SFPro
+          font-size: 13
+          font-weight: normal
+          foreground-color: None
+          background-color: None
+          margin-top: 0
+          margin-bottom: 0
+          margin-left: 0
+          margin-right: 0
+'''
+
 class AutoLayoutWindow:
     def __init__(self, interface='interface.yaml', presentation='presentation.yaml', title='Untitled', during=print):
-        with open(interface, 'r') as f:
-            interface_text = f.read()
-        with open(presentation, 'r') as f:
-            presentation_text = f.read()
-        with open('presentation_defaults.yaml', 'r') as f:
-            presentation_defaults_text = f.read()
+        if isinstance(interface, collections.Mapping):
+            print('Loading AutoLayoutWindow interface in inline dict mode.')
+            self.interface_obj = interface
+        elif isinstance(interface, str):
+            if interface[len(interface) - 5:len(interface)] == '.yaml':
+                print('Loading AutoLayoutWindow interface in YAML file mode.')
+                with open(interface, 'r') as f:
+                    interface_text = f.read()
+                self.interface_obj = yamliny.loads(interface_text.strip())
+            else:
+                print('Loading AutoLayoutWindow interface in inline YAML mode.')
+                self.interface_obj = yamliny.loads(clean_inline_yaml(interface))
+        else:
+            ValueError('Failed to identify an interface YAML file or dict passed to interface argument.')
 
-        self.interface_obj = yamliny.loads(interface_text.strip())
-        self.presentation_obj = yamliny.loads(presentation_text.strip())
+        if isinstance(presentation, collections.Mapping):
+            print('Loading AutoLayoutWindow presentation in inline dict mode.')
+            self.presentation_obj = presentation
+        elif isinstance(presentation, str):
+            if presentation[len(presentation) - 5:len(presentation)] == '.yaml':
+                print('Loading AutoLayoutWindow presentation in YAML file mode.')
+                with open(presentation, 'r') as f:
+                    presentation_text = f.read()
+                self.presentation_obj = yamliny.loads(presentation_text.strip())
+            else:
+                print('Loading AutoLayoutWindow presentation in inline YAML mode.')
+                self.presentation_obj = yamliny.loads(clean_inline_yaml(presentation))
+        else:
+            ValueError('Failed to identify an presentation YAML file or dict passed to presentation argument.')
 
         # Default stylesheet
-        self.presentation_defaults_obj = yamliny.loads(presentation_defaults_text.strip())
+        # Move to dict within .py file at some point
+        self.presentation_defaults_obj = yamliny.loads(clean_inline_yaml(presentation_defaults))
 
         # Set up cursor dictionary
 
@@ -39,10 +141,9 @@ class AutoLayoutWindow:
             self.auto_layout = True
 
         # Widget dictionary. Used for selecting widgets after window has been created.
-        self.element_dictionary = {}
-
-        print(self.interface_obj)
-        print(self.presentation_obj)
+        self.element_name_dictionary = {}
+        self.element_set_dictionary = {}
+        self.element_kind_dictionary = {}
 
         self.root = Tk()
         self.root.title(title)
@@ -52,7 +153,7 @@ class AutoLayoutWindow:
         content_width = 0
 
         if self.auto_layout:
-            content_width = int( self.window_style['width'] )
+            content_width = int(self.window_style['width'])
 
         for line in self.interface_obj.values():
             # Initiate variables to capture line-level style attributes
@@ -73,10 +174,8 @@ class AutoLayoutWindow:
 
         self.root.geometry(f'{content_width}x{content_height}')
 
-        self.root.after(0, lambda:during(self))
+        self.root.after(0, lambda: during(self))
         self.root.mainloop()
-
-
 
     # Cursor functions
 
@@ -101,12 +200,10 @@ class AutoLayoutWindow:
     def _cursor_carriage_return(self, line_height):
         self.cursor['y'] += line_height
         self.cursor['x'] = int(self.window_style['margin-left'])
+
     # Private methods (General)
 
-    # Line height = max ( margin top + height + margin bottom)
-    # For margin top, reset y cursor after each element, then at end of line drop down by the entire line height
-
-    # Private methods (element rendering)
+    # Private methods (cascading styles)
 
     def _get_styles_by_elem(self, kind, style_obj={}):
         new_style_obj = style_obj
@@ -165,6 +262,8 @@ class AutoLayoutWindow:
         }
         return kwag
 
+    # Private methods (element rendering)
+
     def _render(self, elem_name, elem, auto_layout_sf):
         render_groups = {
             'header': self._render_text,
@@ -179,9 +278,10 @@ class AutoLayoutWindow:
         kwag['text'] = elem['text']
         kwag['anchor'] = 'nw'
         kwag['wraplength'] = styles['width']
-        self.element_dictionary[elem['name']] = Label(self.root, **kwag)
+        self.element_name_dictionary[elem['name']] = Label(self.root, **kwag)
+        self._index_element(self.element_name_dictionary[elem['name']], elem)
         elem_size = {
-            'height': int(kwag['font'].metrics('linespace')),
+            'height': int(kwag['font'].metrics('linespace') * 1.1 + 3),
             'width': int(styles['width'])
         }
         line_length = kwag['font'].measure(kwag['text'])
@@ -192,14 +292,16 @@ class AutoLayoutWindow:
             'height': int(styles['margin-top']) + elem_size['height'] + int(styles['margin-bottom']),
             'width': int(styles['margin-left']) + elem_size['width'] + int(styles['margin-right'])
         }
-        self.element_dictionary[elem['name']].place(**self._cursor(elem_dimensions=elem_size, styles=styles), height=elem_size['height'], width=elem_size['width'])
+        self.element_name_dictionary[elem['name']].place(**self._cursor(elem_dimensions=elem_size, styles=styles),
+                                                         height=elem_size['height'], width=elem_size['width'])
         return outer_dimensions  # Line height
 
     def _render_button(self, elem_kind, elem, auto_layout_sf):
         styles = self._get_styles(elem_kind, elem, auto_layout_sf)
         kwag = self._styles_to_arguments(styles)
         kwag['text'] = elem['text']
-        self.element_dictionary[elem['name']] = Button(self.root, **kwag)
+        self.element_name_dictionary[elem['name']] = Button(self.root, **kwag)
+        self._index_element(self.element_name_dictionary[elem['name']], elem)
         elem_size = {
             'height': int(styles['height']),
             'width': int(styles['width'])
@@ -210,8 +312,11 @@ class AutoLayoutWindow:
             'height': int(styles['margin-top']) + elem_size['height'] + int(styles['margin-bottom']),
             'width': int(styles['margin-left']) + elem_size['width'] + int(styles['margin-right'])
         }
-        self.element_dictionary[elem['name']].place(**self._cursor(elem_dimensions=elem_size, styles=styles), height=elem_size['height'], width=None)
+        self.element_name_dictionary[elem['name']].place(**self._cursor(elem_dimensions=elem_size, styles=styles),
+                                                         height=elem_size['height'], width=None)
         return outer_dimensions  # Line height
+
+    # Private methods (to support element rendering)
 
     def _get_line_scale_factor(self, line_items, content_width):
         if self.auto_layout:
@@ -228,13 +333,32 @@ class AutoLayoutWindow:
         else:
             return 1
 
+    def _index_element(self, elem, meta):
+        if 'kind' in meta:
+            kind = meta['kind']
+            elems_list = self.element_kind_dictionary.setdefault(kind, [])
+            elems_list.append(elem)
+        if 'set' in meta:
+            set = meta['set']
+            self.element_set_dictionary[set] = elem
+
     # Public methods (DOM access)
 
     def get_elem_by_name(self, name):
-        return self.element_dictionary[name]
+        return self.element_name_dictionary[name]
 
     def get_elems_by_set(self, set):
-        return set
+        return self.element_set_dictionary[set]
+
+    def get_elems_by_kind(self, kind):
+        return ElemCollection(self.element_kind_dictionary[kind])
+
+    def get_elems_by_line(self, line):
+        lines = self.interface_obj
+        elem_collection = ElemCollection()
+        if line in lines:
+            elems = [self.get_elem_by_name(name) for name in lines[line].keys()]
+            elem_collection = ElemCollection(elems)
+        return elem_collection
 
     # Public methods (DOM manipulation)
-
